ef34e030ad61ecb1119aafb13d0c7d934c6f0955
Mohit Thapa <thapamohit2058@gmail.com>
2025-08-29 23:19:16 +0545

    added a beautiful sidebar

--- src/app/create-new-trip/page.tsx
+++ src/app/create-new-trip/page.tsx
@@ -1,13 +1,139 @@
-import SignOutButtons from '@/components/signoutbutton'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import {
+  IconArrowLeft,
+  IconBrandTabler,
+  IconSettings,
+  IconUserBolt,
+} from "@tabler/icons-react";
+import { motion } from "motion/react";
+import { cn } from "@/utils";
 
-function CreateNewTrip() {
+ function SidebarDemo() {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "#",
+      icon: (
+        <IconBrandTabler className="h-5 w-5 shrink-0 text-neutral-700 " />
+      ),
+    },
+    {
+      label: "Profile",
+      href: "#",
+      icon: (
+        <IconUserBolt className="h-5 w-5 shrink-0 text-neutral-700 " />
+      ),
+    },
+    {
+      label: "Settings",
+      href: "#",
+      icon: (
+        <IconSettings className="h-5 w-5 shrink-0 text-neutral-700 " />
+      ),
+    },
+    {
+      label: "Logout",
+      href: "#",
+      icon: (
+        <IconArrowLeft className="h-5 w-5 shrink-0 text-neutral-700 " />
+      ),
+    },
+  ];
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-       <Link href="/">TripBuddy</Link>
-        <SignOutButtons /></div>
-  )
+    <div
+      className={cn(
+        "mx-auto flex w-full  flex-1 flex-col overflow-hidden  border border-neutral-200 bg-gray-100 md:flex-row  ",
+        "h-screen", // for your use case, use `h-screen` instead of `h-[60vh]`
+      )}
+    >
+      <Sidebar open={open} setOpen={setOpen}>
+        
+        <SidebarBody className="justify-between gap-10">
+          
+          <div className="flex flex-1 flex-col overflow-x-hidden overflow-y-auto">
+            {open ? <Logo /> : <LogoIcon />}
+            <div className="mt-8 flex flex-col gap-2">
+              {links.map((link, idx) => (
+                <SidebarLink key={idx} link={link} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <SidebarLink
+              link={{
+                label: "Manu Arora",
+                href: "#",
+                icon: (
+                  <img
+                    src="https://assets.aceternity.com/manu.png"
+                    className="h-7 w-7 shrink-0 rounded-full"
+                    width={50}
+                    height={50}
+                    alt="Avatar"
+                  />
+                ),
+              }}
+            />
+          </div>
+        </SidebarBody>
+      </Sidebar>
+      <Dashboard />
+    </div>
+  );
 }
+export const Logo = () => {
+  return (
+    <a
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+    >
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black " />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium whitespace-pre text-black "
+      >
+        Acet Labs
+      </motion.span>
+    </a>
+  );
+};
+export const LogoIcon = () => {
+  return (
+    <a
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black"
+    >
+      <div className="h-5 w-6 shrink-0 rounded-tl-lg rounded-tr-sm rounded-br-lg rounded-bl-sm bg-black " />
+    </a>
+  );
+};
 
-export default CreateNewTrip
+
+
+
+
+// Dummy dashboard component with content
+const Dashboard = () => {
+  return (
+    <div className="flex flex-1">
+      <div className="relative h-full w-full border border-neutral-200 shadow-2xs ">
+        <div
+    className="absolute inset-0 z-0"
+    style={{
+      background: "#f5f5f5",
+      backgroundImage: "radial-gradient(circle at 1px 1px, rgba(0, 0, 0, 0.08) 1px, transparent 0)",
+      backgroundSize: "20px 20px",
+    }}
+  />
+       
+      </div>
+    </div>
+  );
+};
+
+
+export default SidebarDemo
