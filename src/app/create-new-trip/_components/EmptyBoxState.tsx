898f542ea92807a6040a72951b5268e391c524d9
Mohit Thapa <thapamohit2058@gmail.com>
2025-09-12 16:30:25 +0545

    ai setup done

---
+++ src/app/create-new-trip/_components/EmptyBoxState.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EmptyBoxState() {
+  return (
+   <div>
+    <h1 className='text-lg font-semibold text-center mt-5'>Start Planning new <span className='text-emerald-600 font-bold text-xl'>Trip</span> with AI</h1>
+   </div>
+  )
+}
+
+export default EmptyBoxState
--- src/app/create-new-trip/_components/chatbot.tsx
+++ src/app/create-new-trip/_components/chatbot.tsx
@@ -4,12 +4,44 @@ import SignOutButtons from '@/components/signoutbutton'
 import { Button } from '@/components/ui/button'
 import { SidebarTrigger } from '@/components/ui/sidebar'
 import { Textarea } from '@/components/ui/textarea'
-import { Send } from 'lucide-react'
-import React from 'react'
+import { Loader2, Send } from 'lucide-react'
+import axios from "axios"
+import React, { useState } from 'react'
+import EmptyBoxState from './EmptyBoxState'
+
+type Message ={
+  role:string,
+  content:string
+}
 
 const Chatbot = () => {
+  const [messages, setMessages] = useState<Message[]>([])
+  const [userInput, setUserInput] = useState<string>()
+  const [loading, setLoading] = useState(false)
 
-  function onSend() {
+  async function onSend() {
+    if(!userInput?.trim()) return;
+    setLoading(true)
+
+    setUserInput('')
+
+    const newMsg:Message={
+      role:'user',
+      content:userInput
+    }
+
+    setMessages((prev:Message[]) =>[...prev, newMsg] )
+    const result  = await axios.post('/api/aimodel', {
+      messages:[...messages, newMsg]
+    });
+
+    setMessages((prev:Message[]) => [...prev, {
+      role:'assistant',
+      content:result.data?.resp
+    }]);
+
+    console.log(result.data)
+    setLoading(false)
 
   }
   return (
@@ -22,25 +54,39 @@ const Chatbot = () => {
     </div>
   </div> 
         </section>
+
+        {messages?.length==0 && 
+        <EmptyBoxState />}
         <section className='p-4 flex-1 overflow-y-auto'>
-          <div className='flex justify-end mt-2'>
+         {messages.map((msg:Message, index) => (
+          msg.role=='user'?
+           <div className='flex justify-end mt-2' key={index}>
             <div className='max-w-lg bg-gray-200 text-neutral-900 font-medium px-2 py-2 rounded-md text-lg'>
-              user msg
+              {msg.content}
             </div>
 
-          </div>
-          <div className='flex justify-start mt-2'>
+          </div>: <div className='flex justify-start mt-2' key={index}>
             <div className='max-w-lg text-neutral-900 font-medium px-2 py-2 text-lg '>
-              Ai message
+              {msg.content}
             </div>
 
           </div>
+         ))}
+
+         {loading && <div className='flex justify-start mt-2' >
+            <div className='max-w-lg text-neutral-900 font-medium px-2 py-2 text-lg '>
+              <Loader2 className='animate-spin' />
+            </div>
+
+          </div>}
+         
         </section>
         <section className='mx-5'>
           
             <div className='relative mb-6 '>
-              <Textarea placeholder='create a trip from newyork to australia' className='w-full h-32   resize-none p-4  rounded-2xl shadow-input border-2 border-neutral-300 bg-white ' />
-              <Button size="icon" className='absolute bottom-6 right-6 bg-teal-600' onClick={() => onSend()}>
+              <Textarea placeholder='Plan Your Trip...' className='w-full h-32   resize-none py-2 px-3  rounded-xl shadow-md  bg-white '
+              onChange={(event) => setUserInput(event.target.value)} value={userInput} />
+              <Button size="icon" className='absolute bottom-3 right-3  cursor-pointer rounded-sm' onClick={() => onSend()}>
                 <Send className='h-4 w-4'/>
               </Button>
             </div>
